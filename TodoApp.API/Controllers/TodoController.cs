7681fc1161c19ac91cf43a7150739a89c484eca2
Dustin Chapman <dchapg@gmail.com>
2024-04-15 08:46:55 -0500

    first commit, adding from local to repo

---
+++ TodoApp.API/Controllers/TodoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.API.Data;
+using TodoApp.API.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoDbContext _todoDbContext;
+
+        public TodoController(TodoDbContext todoDbContext)
+        {
+            _todoDbContext = todoDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTodos()
+        {
+            var todos = await _todoDbContext.Todos.ToListAsync();
+
+            return Ok(todos);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddTodo(Todo todo)
+        {
+            todo.Id = Guid.NewGuid();
+
+            _todoDbContext.Todos.Add(todo);
+            await _todoDbContext.SaveChangesAsync();
+
+            return Ok(todo);
+        }
+    }
+}
+
